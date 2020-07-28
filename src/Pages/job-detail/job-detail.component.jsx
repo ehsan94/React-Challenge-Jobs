038b16a07374c02bd3957e510f61f436ef4e5661
Ehsan <ehsan@user12s-MacBook-Pro.local>
2020-07-28 17:46:59 +0200

    details page fix

--- src/Pages/job-detail/job-detail.component.jsx
+++ src/Pages/job-detail/job-detail.component.jsx
@@ -1,30 +1,41 @@
-import React from 'react';
+import React from "react";
 
-import './job-detail.styles.scss'
+import "./job-detail.styles.scss";
 
-import {selectCurrentJobDetail} from '../../Redux/job/job.selectors'
+import { selectCurrentJobDetail } from "../../Redux/job/job.selectors";
 
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import {connect} from "react-redux";
-import {createStructuredSelector} from 'reselect'
-
-const JobDetail = ({currentJobDetail: { title, city, employer, requirements, tasks}}) =>{
+const JobDetail = ({
+  currentJobDetail: { title, city, employer, requirements, tasks },
+}) => {
    
-    return(
-        <div className='display-item'>
-        <h1 className="title" >{title}</h1>
-        <hr></hr>
-        <h3 >Location : {city}</h3>
-        <h3 >Employer : {employer}</h3>
-        <h3>Requirements :</h3>
-       { requirements.map( (item, index)=> <p key={index} >{item}</p>)}
-       <h3>Tasks :</h3>
-       { tasks.map( (item, index)=> <p key={index} >{item}</p>)}
-        </div>
-    )
-}
+  return (
+    <div className="display-item">
+      <h1 className="title">{title}</h1>
+      <hr></hr>
+      <h3>Location : {city}</h3>
+      <h3>Employer : {employer}</h3>
+      <h3>Requirements :</h3>
+      { requirements !== "NA" ?
+          requirements.map((item, index) => (
+        <p key={index}>{item}</p>
+      )): <p>NA</p>
+    }
+      <h3>Tasks :</h3>
+      {
+        tasks !== "NA" ?
+          tasks.map((item, index) => (
+        <p key={index}>{item}</p>
+      )): <p>NA</p>
+    
+    }
+    </div>
+  );
+};
 const mapStateToProps = createStructuredSelector({
-    currentJobDetail: selectCurrentJobDetail,
-  });
+  currentJobDetail: selectCurrentJobDetail,
+});
 
 export default connect(mapStateToProps)(JobDetail);
